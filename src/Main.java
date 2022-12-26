d3226f33aae48af9f4af55ac27e63769faeb60c3
GusevaAS <118854105+GusevaAS@users.noreply.github.com>
2022-12-26 12:17:43 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 7500;
         int mile = 20;
-        int bonus = amount/mile;
+        int bonus = amount / mile;
         System.out.println(bonus + " миль");
     }
 }
